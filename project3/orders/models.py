812713061440d04007f06efe2023c8a04ce19d8f
Catyousha <jakaasha@gmail.com>
2020-06-22 16:42:31 +0700

    add pizza and topping table

--- project3/orders/models.py
+++ project3/orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    category = models.CharField(max_length=16) #Regular or Sicilian
+    name = models.CharField(max_length=64) #Cheese
+    small = models.DecimalField(max_digits=4, decimal_places=2, null=True) #12.70
+    big = models.DecimalField(max_digits=4, decimal_places=2, null=True) #17.95
+    picture = models.CharField(max_length=255) #image links
+
+    def __str__(self):
+        return f"{self.name}: [Small: {self.small}] [Big: {self.big}]"
+
+class Topping(models.Model):
+    name = models.CharField(max_length=32)
+    pizza = models.ManyToManyField(Pizza, blank=True, related_name="toppings")
+
+    def __str__(self):
+        return f"{self.name}"
